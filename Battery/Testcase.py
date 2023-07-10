1c3bddaecb92e607f0c07fc205388233f2c0c558
vikasprasad7 <138665389+vikasprasad7@users.noreply.github.com>
2023-07-10 18:54:22 +0530

    Create Testcase.py

---
+++ Battery/Testcase.py
@@ -0,0 +1,11 @@
+import unittest
+
+class TestSum(unittest.TestCase):
+  def test_year(self):
+    self.assertEqual(diff(current_date, last_service_date), 2)
+
+  def test_diff(self):
+    self.assertEqual(diff(current_date,last_service_date), 4)
+
+if __name__ == '__main__':
+    unittest.main()
