208f25ac57a190f446b37c7df39bbf014eb92453
vikasprasad7 <138665389+vikasprasad7@users.noreply.github.com>
2023-07-11 11:34:11 +0530

    Update Testcase.py
    updating battery service criteria

--- Battery/Testcase.py
+++ Battery/Testcase.py
@@ -2,7 +2,7 @@ import unittest
 
 class TestSum(unittest.TestCase):
   def test_year(self):
-    self.assertEqual(diff(current_date, last_service_date), 2)
+    self.assertEqual(diff(current_date, last_service_date), 3)
 
   def test_diff(self):
     self.assertEqual(diff(current_date,last_service_date), 4)
